cd9f7f2de684ecab655a438bde731f9aed2f7d3a
luislortega <leon.luisgerardo775@gmail.com>
2019-10-05 17:32:23 -0400

    Project files

---
+++ Mysql_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "mysql.hostinger.es");// Nombre del host
+define("DATABASE", "u101947554_alumn"); // Nombre de la base de datos
+define("USERNAME", "u101947554_root2"); // Nombre del usuario
+define("PASSWORD", "1298Luis"); // Nombre de la constraseña
+?>
